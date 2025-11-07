7f5c9e3e7c66243e0f2a5f86722205e0c47a50f2
Sergio Barriga <sergio@llos.co>
2025-11-07 11:12:37 +0100

    fixes config

--- stores/site.js
+++ stores/site.js
@@ -1,12 +1,14 @@
 import { defineStore } from 'pinia'
 import { useRuntimeConfig } from '#app'
-
+const siteUrl = process.env.NUXT_PUBLIC_SITE_URL
+const API = process.env.API
 export const useSiteStore = defineStore('site', {
   state: () => {
     const config = useRuntimeConfig()
+    const API_PREVIEW = config.public.api
     return {
-      api: config.public.api,
-      url: config.public.siteUrl,
+      api: API || API_PREVIEW,
+      url: siteUrl,
       overflowHidden: false,
     }
   },
