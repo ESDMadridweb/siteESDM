3e031a96db3d82648ebc0d46cb47f50ae35195c8
ESDMadridweb <comunicacion@esdmadrid.es>
2026-02-02 12:34:19 +0100

    cambios css

--- components/flexibles/SalidasProfesionales.vue
+++ components/flexibles/SalidasProfesionales.vue
@@ -4,7 +4,7 @@
         <div v-html="block?.texto" class="text-BO2 lg:text-T04 indent-[25%] lg:indent-[41.6%] mb-10 lg:mb-14 font-book" />
         <div class="grid grid-cols-1 lg:grid-cols-12">
             <div class="lg:col-start-6 lg:col-span-7 grid grid-cols-1 lg:grid-cols-2 gap-x-5 gap-y-3 text-BO3 px-6 lg:px-0">
-                <div v-for="item in block?.lista" class="relative before:absolute before:top-1/2 before:-translate-y-1/2 before:-left-5 before:content-[''] before:w-2 lg:before:w-3 before:h-2 lg:before:h-3 before:bg-black before:rounded-full">{{ item?.texto }}</div>
+                <div v-for="item in block?.lista" class="relative before:absolute before:top-1/2 before:-translate-y-1/2 before:-left-5 flex items-center npm run dbefore:content-[''] before:w-2 lg:before:w-3 before:h-2 lg:before:h-3 before:bg-black before:rounded-full">{{ item?.texto }}</div>
             </div>
         </div>
         
